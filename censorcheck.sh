9b0b9b324619d4c753b01e6abe8e1945db532de3
Andrey Timoschuk <57887634+AndreyTimoschuk@users.noreply.github.com>
2026-08-31 13:43:04 +0200

    Change RIPE_API_KEY in censorcheck.sh
    Updated RIPE_API_KEY to a new value.

--- censorcheck.sh
+++ censorcheck.sh
@@ -15,7 +15,7 @@ PROXY=""
 VERBOSE=false
 DEBUG=false
 
-RIPE_API_KEY="5a5edf32-179b-4783-b2a4-e96b0506ce36" # Не берите ключ, пожалуйста, можете создать свой на atlas.ripe, это не сложно
+RIPE_API_KEY="SktRUU5ZVFpCWklaTkNGTUhERVVIVEFYOk4xU2ZGTWhqSlo1Z0h2TDN2a2doaG1EYg==" # Не берите ключ, пожалуйста, можете создать свой на atlas.ripe, это не сложно
 REALITY_SNI="max.ru"
 
 while [[ $# -gt 0 ]]; do
