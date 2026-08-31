abcae1aa03baaa3591a9212a4071e1ca173534d0
Andrey Timoschuk <57887634+AndreyTimoschuk@users.noreply.github.com>
2026-08-31 13:40:00 +0200

    Update RIPE_API_KEY in censorcheck.sh
    Replaced placeholder RIPE_API_KEY with actual key.

--- censorcheck.sh
+++ censorcheck.sh
@@ -15,7 +15,7 @@ PROXY=""
 VERBOSE=false
 DEBUG=false
 
-RIPE_API_KEY="Insert the key" # Не берите ключ, пожалуйста, можете создать свой на atlas.ripe, это не сложно
+RIPE_API_KEY="5a5edf32-179b-4783-b2a4-e96b0506ce36" # Не берите ключ, пожалуйста, можете создать свой на atlas.ripe, это не сложно
 REALITY_SNI="max.ru"
 
 while [[ $# -gt 0 ]]; do
